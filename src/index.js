23aaa8df25fe841040d23c0034bc60322dd9a65d
Rubén Sánchez <sanchezramirez.rubendario>
2020-04-30 14:14:40 -0500

    ESLint

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWorld from './componets/HelloWorld'
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('app')
+);
